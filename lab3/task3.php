5faf46bbb69f6b19cdbc4772bc14dee3084847f7
fahim <forhad.fahim826@gmail.com>
2021-06-14 09:48:06 +0600

    php task

---
+++ lab3/task3.php
@@ -0,0 +1,15 @@
+<?php
+$length="10";
+$width="5";
+$area= $length*$width;
+
+echo"area of rectangle is $area";
+
+if($length === $width){
+		echo "the shape is a square";
+	}
+
+
+
+
+?>
---
+++ lab3/task5.php
@@ -0,0 +1,23 @@
+<?php
+
+$backColor="green" ;
+$fontColor="red" ;
+$imageWidth= "80px" ;
+$imageHeight="80" ;
+
+?>
+<table>
+  <tr>
+
+    <td>
+	<img src = "aiub_logo.jpg" width="<?php echo "$imageWidth" ?>" height=""<?php echo "$imageHeight" ?>""/>
+	</td>
+	
+	<td>
+     <h2> American international University -<span style = "background-color:<?php echo "$backColor" ?>; color:<?php echo "$fontColor" ?>">
+      Bangladesh </h2>
+      </span>
+	  </td>
+	  
+	  </tr>
+</table>
